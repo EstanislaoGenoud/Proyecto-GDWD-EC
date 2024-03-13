4bb966775ccfa522676febb1fef734f0490c5d81
Estanislao Genoud <estangeno@gmail.com>
2024-03-12 23:18:11 -0300

    Preparando para pasar todo a JS

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btnCart = document.querySelector('.btnCart');
+    const CartCount = document.querySelector('#cartCount');
+    const contentIlustration = document.querySelector('.ilustration');
+    const contentRecommended = document.querySelector('.recommended');
+    console.log("🚀 ~ document.addEventListener ~ contentRecommended:", contentRecommended)
+})
+
+
